a4f0fcd33efdeb219a14c64b7d269f17b03d9202
RajaJamal913 <m.jamalraja436@gmail.com>
2025-04-26 19:52:36 +0500

    first commit

--- resume/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ResumeUploadView, ResumeParseView
-
-urlpatterns = [
-    path('upload/', ResumeUploadView.as_view(), name='resume-upload'),
-    path('parse/<int:pk>/', ResumeParseView.as_view(), name='resume-parse'),
-]
